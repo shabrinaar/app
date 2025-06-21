6b026dcee9bc12c7f5a5994571815f04ff05aef0
shabrinaar <162359022+shabrinaar@users.noreply.github.com>
2025-06-21 15:43:29 +0700

    app.py

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ st.write("Pilih tanggal untuk memprediksi jumlah Unit A yang terjual.")
 # Input tanggal
 tanggal = st.date_input("Pilih Tanggal", datetime.today())
 
+# Ambil hari dari tanggal (0=Senin, ..., 6=Minggu)
+day_of_week = tanggal.weekday()
+
 # Nilai default fitur lain (misal dari data training, bisa disesuaikan)
 default_grp_a = 2.47
 default_grp_b = 0.07
@@ -32,7 +35,7 @@ if st.button("Prediksi"):
         'Unit_B': default_unit_b,
         'GRP_A_adstock': default_grp_a,
         'GRP_B_adstock': default_grp_b,
-        'Day': default_day,
+        'Day': day_of_week,
         'Toko1': default_toko1,
         'Toko3': default_toko3,
         'Toko4': default_toko4,
